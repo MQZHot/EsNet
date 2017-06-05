af5b133b085a22c1d49eaef37be8ae8adef9a7c6
mqz <mqz1228@163.com>
2017-06-05 18:23:42 +0800

    修改demo

--- EsNet/ViewController.swift
+++ EsNet/ViewController.swift
@@ -13,30 +13,41 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var imgView: UIImageView!
     
+    @IBOutlet weak var `switch`: UISwitch!
+    @IBOutlet weak var dataLabel: UILabel!
+    @IBOutlet weak var cachelabel: UILabel!
+    
     let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        picProvider.requestJson(.getPic(type: "20"), isCache: true)
+        request(cache: true)
+    }
+    
+    func request(cache: Bool) -> Void {
+        picProvider.requestJson(.getPic(type: "20"), isCache: cache)
             .mapArray(type: AdModel.self)
             .subscribe( onNext: { (modelArr) in
                 
                 if modelArr.count != 0 {
                     let model = modelArr.first
                     let url = model?.url ?? ""
+                    let name = model?.name ?? ""
                     self.imgView.kf.setImage(with: URL(string: url))
-                    
-                    
+                    self.cachelabel.text = cache ? "\(name)\n\(url)" : ""
+                    self.dataLabel.text = "\(name)\n\(url)"
                 }
                 
-        })
+            })
             .addDisposableTo(disposeBag)
+    }
+    
+    @IBAction func switchClick(_ sender: UISwitch) {
         
-        
+        request(cache: sender.isOn)
         
     }
+    
     
 }
 
---
+++ README.md
@@ -0,0 +1,15 @@
+# EsNet
+
+Swift netRequest & cache --- Moya+RxSwift+ObjectMapper+AwesomeCache  
+
+网络请求 + 缓存  --- Moya+RxSwift+ObjectMapper+AwesomeCache  
+![](https://img.shields.io/badge/platform-iOS-red.svg)[![Language](https://img.shields.io/badge/language-Swift-brightgreen.svg)](https://github.com/MQZHot/EsNet)
+
+# EsNet
+
+##### Swift 网络请求 + 缓存 Moya+RxSwift+ObjectMapper+AwesomeCache  
+
+![image](https://github.com/MQZHot/EsNet/raw/master/111.gif)
+
+
+
